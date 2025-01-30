92a017b9d6730d031f7aa30a9bd12081bb607184
Martin Barhaug <martin.barhaug@norconsult.com>
2025-01-30 19:01:03 +0100

    La til login og logut

--- Authentication and Authorization dotnetcore/m4/Finished/Globomantics/Controllers/ProposalController.cs
+++ Authentication and Authorization dotnetcore/m4/Finished/Globomantics/Controllers/ProposalController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Globomantics.Controllers;
+[Authorize]
 public class ProposalController : Controller
 {
     private readonly IConferenceRepository conferenceRepo;
